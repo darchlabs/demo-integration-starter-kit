cd9a86b597c6a88b32c561b8f019ee71a213a8a0
Camilo Acuña <camilo.aku@gmail.com>
2023-09-28 02:52:26 +0200

    feat(demo): add webhooks essentials tutorial demo

---
+++ webhooks-essentials/src/index.ts
@@ -0,0 +1,18 @@
+import { util, webhooks } from "darchlabs";
+import dotenv from 'dotenv';
+
+// Load environment variables from a .env file
+dotenv.config();
+
+// Ensure necessary environment variables are set
+if (!process.env.PORT) {
+	throw new Error("missing necessary environment variables. Ensure PORT is set.");
+}
+
+const main = async () => {
+	util.ListenServer(Number(process.env.PORT), "/api/v1/webhook", async (wh: webhooks.Webhook<unknown>) => {
+		console.log("> webhook received!", wh.id)
+	});
+}
+
+main();
