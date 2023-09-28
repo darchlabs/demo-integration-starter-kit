b54a3c5833d122c373a84129c0a5c6c32c10aa43
Camilo Acuña <camilo.aku@gmail.com>
2023-09-28 02:53:18 +0200

    remove files

--- webhooks-express/index2.js
+++
@@ -1,6 +0,0 @@
-const Darchlabs = require("darchlabs")
-const { webhooks } = new Darchlabs.default("token")
-
-webhooks.listen(3002, "/api/v1/webhook", (wh) => {
-  console.log("here in webhook", wh)
-})
